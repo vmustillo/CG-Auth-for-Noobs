89e9a975718e88d362721e78fcfff0b4b056e632
vmustillo <vjmustillo@gmail.com>
2018-10-29 20:43:47 -0400

    Enabled hashing passwords with bcrypt.  Successfully implemented posting users to database

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const Joi = require('joi');
+const bcrypt = require('bcryptjs');
 const router = express.Router();
 
 const db = require('../db/connection.js');
 const users = db.get('users');
-users.index('username');
+users.createIndex('username', { unique: true });
 
 const schema = Joi.object().keys({
   username: Joi.string()
     .regex(/(^[a-zA-Z0-9_]*$)/)
-    .min(8)
+    .min(2)
     .max(30)
     .required(),
   password: Joi.string()
@@ -24,13 +25,39 @@ router.get('/', (req, res) => {
 });
 
 //POST /auth/signup
-router.post('/signup', (req, res) => {
-  console.log('body', req.body);
+router.post('/signup', (req, res, next) => {
   const result = Joi.validate(req.body, schema);
-
-  res.json({
-    result
-  });
+  if (result.error === null) {
+    // make sure username is unique
+    users
+      .findOne({
+        username: req.body.username
+      })
+      .then(user => {
+        // if user is undefined, username is not in DB
+        if (user) {
+          // there is already user in DB with this username
+          const error = new Error(
+            'This username already exists. Please enter a new username'
+          );
+          next(error);
+        } else {
+          // hash password
+          // insert user with hashed password
+          bcrypt.hash(req.body.password, 12).then(hashedPassword => {
+            const newUser = {
+              username: req.body.username,
+              password: hashedPassword
+            };
+            users.insert(newUser).then(insertedUser => {
+              res.json(insertedUser);
+            });
+          });
+        }
+      });
+  } else {
+    next(result.error);
+  }
 });
 
 module.exports = router;
--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const volleyball = require('volleyball');
-const bodyParser = require('body-parser');
 
 const app = express();
 
