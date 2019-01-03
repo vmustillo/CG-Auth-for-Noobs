55268bf893e46f65bd0991cf542ee3ca2296bb21
vmustillo <vjmustillo@gmail.com>
2019-01-02 22:57:00 -0500

    If the user is logged in and valid, the username displays on the dashboard.  If not, the user is prompted to logout and is redirected to the login page

--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -1,21 +1,39 @@
 <template>
-  <h1>Dashboard</h1>
+  <section>
+    <h1>Dashboard</h1>
+    <h1 v-if="!user">Getting user information...</h1>
+    <h1 v-if="user">Hello, {{user.username}}</h1>
+    <button @click="logout()" class="btn btn-primary">Logout</button>
+  </section>
 </template>
 
 <script>
-const API_URL = "http://localhost:5000/";
+const API_URL = 'http://localhost:5000/';
 
 export default {
+  data: () => ({
+    user: {}
+  }),
   mounted() {
     fetch(API_URL, {
       headers: {
-        authorization: `Bearer ${localStorage.token}`,
+        authorization: `Bearer ${localStorage.token}`
       },
     })
       .then(res => res.json())
       .then(result => {
-        console.log(result);
+        if (result.user) {
+          this.user = result.user;
+        } else {
+          this.logout();
+        }
       });
+  },
+  methods: {
+    logout() {
+      localStorage.removeItem('token');
+      this.$router.push('/login');
+    },
   },
 };
 </script>
