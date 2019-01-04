d57b19c5df9caeeaf1bfbbb0a341e31401de9dc1
vmustillo <vjmustillo@gmail.com>
2019-01-04 00:25:38 -0500

    Created form on dashboard to insert new notes.  Note is then passed to the backend and inserted into the database. Get to api/v1/notes now returns all notes from logged in user

--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -4,6 +4,35 @@
     <h1 v-if="!user">Getting user information...</h1>
     <h1 v-if="user">Hello, {{user.username}}</h1>
     <button @click="logout()" class="btn btn-primary">Logout</button>
+    <br/>
+    <br/>
+    <button @click="showForm = !showForm" class="btn btn-info">Toggle Form</button>
+    <form v-if="showForm" @submit.prevent="addNote()">
+      <div class="form-group">
+        <label for="title">Title</label>
+        <input 
+          v-model="newNote.title" 
+          type="text" 
+          class="form-control" 
+          id="title" 
+          aria-describedby="titleHelp" 
+          placeholder="Enter title" 
+          required>
+        <small id="titleHelp" class="form-text text-muted">Enter a descriptive title for your note.</small>
+      </div>
+      <div class="form-group">
+        <label for="description">Note</label>
+        <textarea 
+          v-model="newNote.note" 
+          class="form-control" 
+          id="note" 
+          rows="3" 
+          placeholder="Enter your note" 
+          required>
+        </textarea>
+      </div>
+      <button type="submit" class="btn btn-success">Add Note</button>
+    </form>
   </section>
 </template>
 
@@ -12,7 +41,13 @@ const API_URL = 'http://localhost:5000/';
 
 export default {
   data: () => ({
-    user: {}
+    showForm: false,
+    user: null,
+    newNote: {
+      title: '',
+      note: '',
+    },
+    notes: [],
   }),
   mounted() {
     fetch(API_URL, {
@@ -30,6 +65,24 @@ export default {
       });
   },
   methods: {
+    addNote() {
+      fetch(`${API_URL}api/v1/notes`, {
+        method: 'POST',
+        body: JSON.stringify(this.newNote),
+        headers: {
+          'content-type': 'application/json',
+          authorization: `Bearer ${localStorage.token}`,
+        },
+      }).then(res => res.json())
+      .then(note => {
+        this.notes.push(note);
+        this.newNote = {
+          title: '',
+          note: '',
+        };
+        this.showForm = false;
+      });
+    },
     logout() {
       localStorage.removeItem('token');
       this.$router.push('/login');
